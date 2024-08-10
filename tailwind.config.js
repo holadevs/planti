d430cb8e4016ab1324b2b132ea0492b21f9e8eaf
holadevs <holadev2024@gmail.com>
2024-08-10 12:43:47 -0500

    init project

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#5da623',
+        background: '#f7f8fa',
+        'light-green': '#c0f397',
+        text: '#dcdddf',
+        nav: '#828282'
+      }
+    },
+  },
+  plugins: [],
+}
+
